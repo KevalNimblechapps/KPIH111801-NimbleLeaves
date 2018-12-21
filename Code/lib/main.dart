2827f1436345d7e4701695ed14144a8a04010788
juned <juned@nimblechapps.com>
2018-12-21 12:12:21 +0530

    login page design integrated

---
+++ Code/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+// import 'package:nimble_leaves/employee_list/employee_list.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(248, 250, 251, 1),
+      body: Center(
+        child: RaisedButton(
+          child: Image.asset('images/login.png'),
+          color: Color.fromRGBO(248, 250, 251, 1),
+          elevation: 0,
+          splashColor: Color.fromRGBO(248, 250, 251, 1),
+          highlightColor: Color.fromRGBO(248, 250, 251, 1),
+          highlightElevation: 0,
+          onPressed: () {
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => SecondScreen()),
+            // );
+          },
+        ),
+      ),
+    );
+  }
+}
